3c0d38b813f648adca36041dac54a69b6384bcf3
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2023-12-12 12:17:18 +0100

    Add files via upload

---
+++ vj2.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		   int a = 10;
+            int b = 7;
+            double c = 12.5;
+            double d = 16.7;
+
+            double prosjek = (a + b + c + d) / 4;
+
+            Console.WriteLine("Prosjek vrijednosti je: " + prosjek);
+	}
+}
